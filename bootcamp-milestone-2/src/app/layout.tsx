bf32cd5176f7e603f861cef636ff5a2cb9f3aa6a
bri8679 <bkirkman@calpoly.edu>
2024-12-22 20:15:33 -0800

    fix: lintfix

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -1,24 +1,12 @@
 import type { Metadata } from "next"; // Object to set metadata
 import { Inter } from "next/font/google"; // You can change the font to anything you want.
 import "./globals.css";
-import localFont from "next/font/local";
+
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
 // If you are experiencing an error "localFont is undefined", you might need to add the following blocks of code
 const inter = Inter({ subsets: ["latin"] });
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-//
 
 export const metadata: Metadata = {
   title: "[name] Personal Website",
@@ -37,7 +25,7 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen">
           {/* Navbar */}
           <Navbar />
-          
+
           {/* Main content */}
           <main className="flex-grow">{children}</main>
 
